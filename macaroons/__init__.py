9f46e439db48b3b42ee0cbbbb913fd1d1e07a231
Evan Cordell <evan.cordell@localmed.com>
2014-09-09 11:36:36 -0500

    Don't import in __init__

--- macaroons/__init__.py
+++ macaroons/__init__.py
@@ -1,6 +1,3 @@
-from macaroons.macaroon import Macaroon
-from macaroons.verifier import Verifier
-
 VERSION = (0, 1, 8)
 __version__ = '.'.join(str(x) for x in VERSION)
 
@@ -8,6 +5,4 @@ __version__ = '.'.join(str(x) for x in VERSION)
 __all__ = [
     'VERSION',
     '__version__',
-    'Macaroon',
-    'Verifier',
 ]
